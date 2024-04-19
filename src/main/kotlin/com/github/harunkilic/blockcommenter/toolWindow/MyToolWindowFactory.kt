c1bd0dba319ba5e06c68f597b4cd8ebd62e3ae5f
GitHub Action <action@github.com>
2024-04-19 09:24:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/harunkilic/blockcommenter/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/harunkilic/blockcommenter/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.harunkilic.blockcommenter.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.harunkilic.blockcommenter.MyBundle
+import com.github.harunkilic.blockcommenter.services.MyProjectService
 import javax.swing.JButton
 
 
